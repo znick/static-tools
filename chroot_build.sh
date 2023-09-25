f8dc75d91057ae3186e4dd4186c3be33bc9e9e79
Nikolay Zhuravlev <znick@znick.ru>
2023-09-25 19:54:14 +0200

    Update chroot_build.sh

--- chroot_build.sh
+++ chroot_build.sh
@@ -30,15 +30,15 @@ sudo cp -p /etc/resolv.conf miniroot/etc/
 
 if [ "$ARCHITECTURE" = "x86" ] || [ "$ARCHITECTURE" = "x86_64" ]; then
     echo "Architecture is x86 or x86_64, hence not using qemu-arm-static"
-    sudo cp build.sh miniroot/build.sh && sudo chroot miniroot /bin/sh -ex /build.sh
+    sudo cp build.sh miniroot/build.sh && sudo -E chroot miniroot /bin/sh -ex /build.sh
 elif [ "$ARCHITECTURE" = "aarch64" ] ; then
     echo "Architecture is aarch64, hence using qemu-aarch64-static"
     sudo cp "$(which qemu-aarch64-static)" miniroot/usr/bin
-    sudo cp build.sh miniroot/build.sh && sudo chroot miniroot qemu-aarch64-static /bin/sh -ex /build.sh
+    sudo cp build.sh miniroot/build.sh && sudo -E chroot miniroot qemu-aarch64-static /bin/sh -ex /build.sh
 elif [ "$ARCHITECTURE" = "armhf" ] ; then
     echo "Architecture is armhf, hence using qemu-arm-static"
     sudo cp "$(which qemu-arm-static)" miniroot/usr/bin
-    sudo cp build.sh miniroot/build.sh && sudo chroot miniroot qemu-arm-static /bin/sh -ex /build.sh
+    sudo cp build.sh miniroot/build.sh && sudo -E chroot miniroot qemu-arm-static /bin/sh -ex /build.sh
 else
     echo "Edit chroot_build.sh to support this architecture as well, it should be easy"
     exit 1
